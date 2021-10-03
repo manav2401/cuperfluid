80b3c67831656d14619f6712f11bd934c63f6115
Manav Darji <manavdarji.india@gmail.com>
2021-10-03 20:33:58 +0530

    added env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,7 +14,7 @@ require("@nomiclabs/hardhat-waffle");
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -23,7 +24,7 @@ module.exports = {
   networks: {
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
-      accounts: ["cd9988efcb7dd3104293f8713645671744a4c43e56780df03a3402dfd7f893e6"],
+      accounts: [PRIVATE_KEY],
     }
   },
   solidity: "0.7.6",
