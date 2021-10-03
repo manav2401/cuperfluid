9f2492452534c852e4a6d7c274b8978f2640370c
Manav Darji <manavdarji.india@gmail.com>
2021-10-04 00:09:52 +0530

    format

--- scripts/chainlink-validate-proof.js
+++ scripts/chainlink-validate-proof.js
@@ -1,15 +1,13 @@
 async function main() {
-	console.log(
-		"Chainlink request to sender for validating Proof of Work"
-	);
+	console.log("Chainlink request to sender for validating Proof of Work");
 	const contractAddress = "0xf933c4323510ED7B518aEb7Ae4A0616DA8c86cb5";
 	console.log("Getting Contract");
 	const Handler = await ethers.getContractFactory("Handler");
 	const handler = await Handler.attach(contractAddress);
 	console.log("Linked with Handler contract deployed at", contractAddress);
 
-    let result = await handler.getResult();
-    console.log("Result before proof:", result);
+	let result = await handler.getResult();
+	console.log("Result before proof:", result);
 
 	// call the validate proof function
 	console.log("Making API request to oracle");
@@ -21,8 +19,8 @@ async function main() {
 	console.log("Waiting for oracle to fulfill the response");
 	await new Promise((resolve) => setTimeout(resolve, 30000));
 
-    result = await handler.getResult();
-    console.log("Result after proof:", result);
+	result = await handler.getResult();
+	console.log("Result after proof:", result);
 }
 
 main()
